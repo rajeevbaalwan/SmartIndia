a83bb9ab1fec584f16b8d2efd4cb0322d566907e
Namit Yadav <namit.yv@gmail.com>
2017-03-30 19:34:55 +0530

    profile activity etc

--- app/src/main/java/com/example/admin/smartindia/Activities/AllergyActivity.java
+++ app/src/main/java/com/example/admin/smartindia/Activities/AllergyActivity.java
@@ -10,6 +10,6 @@ public class AllergyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_allergy_custom_single_row);
+        setContentView(R.layout.activity_allergy);
     }
 }
