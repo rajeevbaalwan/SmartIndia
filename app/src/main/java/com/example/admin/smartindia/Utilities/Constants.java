527fe67137605b84082729e668c43095acb3120f
rahulyadav20111995 <rahulyadav574@gmail.com>
2017-04-01 10:24:47 +0530

    server ip change

--- app/src/main/java/com/example/admin/smartindia/Utilities/Constants.java
+++ app/src/main/java/com/example/admin/smartindia/Utilities/Constants.java
@@ -4,8 +4,8 @@ package com.example.admin.smartindia.Utilities;
  * Created by admin on 24/03/2017.
  */
 public interface Constants {
-    String BASE_URL="http://192.168.43.121:5000/api/";
-    String SOCKET_SERVER = "http://192.168.43.121:3000/";
+    String BASE_URL="http://10.1.152.244:5000/api/";
+    String SOCKET_SERVER = "http://10.1.152.244:3000/";
     String EVENT_REGISTER_USER = "new_user_req";
     String EVENT_NOTIFY = "notify_user";
 }
